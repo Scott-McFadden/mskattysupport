48765c5bf48bba1f714cde48f26b25a0e557ab56
improvingcomputers <improvingcomputers@yahoo.com>
2020-06-14 11:30:12 -0500

    v 0.1 commit

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const http = require('http');
+const express = require('express');
+const app = express();
+const GracefulShutdownManager = require('@moebius/http-graceful-shutdown').GracefulShutdownManager;
+
 const hostname = '127.0.0.1';
 const port = 3000;
+const shutDownTimeOut = 3000;
 
-const server = http.createServer((req,res)  => {
-    res.statusCode=200;
-    res.setHeader("Content-Type","text/plain");
-    res.end("Hello World");
+app.get('/', (req, res) => {
+    res.send("Hi!");
 });
 
-server.listen(port, hostname, () => {
-   console.log(`Server Running at http://${hostname}:${port}/`);
 
+
+
+app.get('/stopServer', (req, res) => {
+    res.send("Thank you - shutting down");
+    console.log("stopServer requested - shutting down");
+    shutdownManager.terminate(() => {
+        console.log('Server is gracefully terminated');
+    });
 
 });
+
+const server = app.listen(port, ()=> {
+    console.log(`server started on ${port}`);
+});
+
+const shutdownManager = new GracefulShutdownManager(server,{ forceTimeout: shutDownTimeOut });
+
